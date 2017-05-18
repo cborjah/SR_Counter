99c29aeda2b9985e4071d8f83e7cf587fd0e8033
Christian Borja <cpborja32@gmail.com>
2017-05-17 23:56:58 -0700

    removes assets from gitignore, refactors mainScreen

--- app/components/mainScreen.js
+++ app/components/mainScreen.js
@@ -14,17 +14,14 @@ class MainScreen extends Component {
       <View style={styles.container}>
         <StatusBar hidden />
         <ScrollableTabView
-          renderTabBar={() => <DefaultTabBar style={{ borderWidth: 0, height: (Responsive.DEVICE_HEIGHT * 0.15) / 2 }} tabStyle={{ paddingBottom: 0, backgroundColor: '#070d18', height: 50 }} activeTextColor='white' inactiveTextColor='#444' textStyle={{ fontSize: Responsive.FONT_SIZE_SMALLER, ...Platform.select({ ios: { letterSpacing: 2.75, fontWeight: '700' } }) }} />}
+          renderTabBar={() => <DefaultTabBar style={styles.main} tabStyle={styles.tab} activeTextColor='white' inactiveTextColor='#444' textStyle={styles.tabText} />}
           tabBarPosition='top'
-          // style={this.props.activeTab ? { backgroundColor: '#212121', transform: [{ rotate: '180deg'}]} : { backgroundColor: '#212121' }}
           style={{ backgroundColor: '#070d18' }}
           tabBarUnderlineStyle={{ backgroundColor: 'white', height: 1 }}
-          // activeTabStyle={{ backgroundColor: '#1E1E1E' }}
           locked={true}
           ref={tabView => { this.tabView = tabView; }}
           onChangeTab={({i, ref}) => this.props.tabChanged(i)}
-          prerenderingSiblingsNumber={1}
-        >
+          prerenderingSiblingsNumber={1}>
           <Player1 changeTab={() => this.tabView.goToPage(1)} tabLabel={this.props.name1} />
           <Player2 changeTab={() => this.tabView.goToPage(0)} tabLabel={this.props.name2} />
         </ScrollableTabView>
@@ -36,6 +33,18 @@ class MainScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  main: {
+    borderWidth: 0,
+    height: (Responsive.DEVICE_HEIGHT * 0.15) / 2
+  },
+  tab: {
+    paddingBottom: 0,
+    backgroundColor: '#070d18',
+  },
+  tabText: {
+    fontSize: Responsive.FONT_SIZE_SMALLER,
+    ...Platform.select({ ios: { letterSpacing: 2.75, fontWeight: '700' } })
   }
 });
 
